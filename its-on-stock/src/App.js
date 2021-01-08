67624ab2f632f59eafbe6544b8551f67492bc9ee
unknown <andrewturpo573@gmail.com>
2021-01-08 00:40:25 -0500

    Project configuration

---
+++ its-on-stock/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Aqui empezamos a escribir codigo
+    </div>
+  );
+}
+
+export default App;
